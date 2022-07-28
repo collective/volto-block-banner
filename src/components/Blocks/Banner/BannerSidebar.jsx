65cd729d37a5147e74a847d76f81d8c2c22bccc8
avimishra18 <mishra.avi18@gmail.com>
2022-07-28 17:59:27 +0530

    🐛  Fixed Edit mode bug

---
+++ src/components/Blocks/Banner/BannerSidebar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BlockDataForm, SidebarPortal } from '@plone/volto/components';
+import { BannerSchema } from '@plone-collective/volto-block-banner/components/Blocks/Banner/schema';
+
+const BannerSidebar = (props) => {
+  const { data, block, onChangeBlock, selected } = props;
+  const schema = BannerSchema();
+
+  return (
+    <SidebarPortal selected={selected}>
+      <BlockDataForm
+        schema={schema}
+        title={schema.title}
+        onChangeField={(id, value) => {
+          onChangeBlock(block, {
+            ...data,
+            [id]: value,
+          });
+        }}
+        formData={data}
+        fieldIndex={data.index}
+        block={block}
+      />
+    </SidebarPortal>
+  );
+};
+
+export default BannerSidebar;
--- src/components/Blocks/Banner/Edit.jsx
+++ src/components/Blocks/Banner/Edit.jsx
@@ -1,31 +1,12 @@
 import React from 'react';
-import { BlockDataForm, SidebarPortal } from '@plone/volto/components';
 import View from '@plone-collective/volto-block-banner/components/Blocks/Banner/View';
-import { BannerSchema } from '@plone-collective/volto-block-banner/components/Blocks/Banner/schema';
+import BannerSidebar from '@plone-collective/volto-block-banner/components/Blocks/Banner/BannerSidebar';
 
 const Edit = (props) => {
-  const { data, block, onChangeBlock, selected } = props;
-
-  const schema = BannerSchema();
-
   return (
     <>
-      <View {...props} />
-      <SidebarPortal selected={selected}>
-        <BlockDataForm
-          schema={schema}
-          title={schema.title}
-          onChangeField={(id, value) => {
-            onChangeBlock(block, {
-              ...data,
-              [id]: value,
-            });
-          }}
-          formData={data}
-          fieldIndex={data.index}
-          block={block}
-        />
-      </SidebarPortal>
+      <View {...props} mode="edit" />
+      <BannerSidebar {...props} />
     </>
   );
 };
